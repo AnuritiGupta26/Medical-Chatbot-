9249cba3b67caf69f7030f8062e57ab297fc7a65
AnuritiGupta26 <anuritigupta26@gmail.com>
2025-02-11 22:50:12 +0530

    updated

--- app.py
+++ app.py
@@ -93,6 +93,4 @@ def home():
     return "Medical Chatbot Running!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port= 8080, debug= True)
